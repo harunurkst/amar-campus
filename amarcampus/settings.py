4f1117dab6195fe519c078c9e08d87142cfbb17b
harunurkst <harun1393@gmail.com>
2017-04-03 10:41:33 +0600

    latest update

--- amarcampus/settings.py
+++ amarcampus/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'campus',
     'student',
     'location',
+    'blog',
 ]
 
 MIDDLEWARE = [
--- amarcampus/views.py
+++ amarcampus/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post, Category
+from student.models import StudentProfile
 
 
 def home(request):
-    context = {}
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
     return render(request, 'home/home.html', context)
